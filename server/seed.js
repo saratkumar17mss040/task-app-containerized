a0c2b90ae29a26b131f3fb3fbadc0f5fa638113d
saratkumar17mss040 <sarathdon9245@gmail.com>
2026-05-30 19:47:38 +0530

    update: mongo url

--- server/seed.js
+++ server/seed.js
@@ -5,7 +5,7 @@ const User = require("./models/user");
 
 const seedUsers = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to MongoDB");
 
     // Delete existing users
